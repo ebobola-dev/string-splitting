f0b7e60a3f0eab89bcb24669ce518d9ea8af800a
ebobola-web <romawot94@gmail.com>
2021-11-19 11:07:13 +0300

    add continue

--- string-splitting/Source.cpp
+++ string-splitting/Source.cpp
@@ -9,81 +9,92 @@ int main() {
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 
-	char inputStr[MAX_INPUT_SIZE + 1] = {'\0', };
-	int lengthStr;
+	char inputStr[MAX_INPUT_SIZE + 1];
+	int lengthStr, cont = 1;
 
 	char letterList[MAX_INPUT_SIZE][MAX_INPUT_SIZE];
 	char digitList[MAX_INPUT_SIZE][MAX_INPUT_SIZE];
 	int letterList_position = 0, digitList_position = 0;
+	char trash;
 
-	printf("Введите что-нибудь(макс %d символов): ", MAX_INPUT_SIZE);
-	fgets(inputStr, MAX_INPUT_SIZE + 1, stdin);
+	while(cont){
+		letterList_position = 0, digitList_position = 0;
+		inputStr[0] = '\0';
 
-	while (strlen(inputStr) == 1) {
-		printf("Вы ничего не ввели\nВведите заново(макс %d): ", MAX_INPUT_SIZE);
+		printf("Введите что-нибудь(макс %d символов): ", MAX_INPUT_SIZE);
 		fgets(inputStr, MAX_INPUT_SIZE + 1, stdin);
-	}
-	lengthStr = strlen(inputStr) - 1;
 
-	int _internalPosition = 0;
-	int _wasChange = 0;
-	char _lastIs;
+		while (strlen(inputStr) == 1) {
+			printf("Вы ничего не ввели\nВведите заново(макс %d): ", MAX_INPUT_SIZE);
+			fgets(inputStr, MAX_INPUT_SIZE + 1, stdin);
+		}
+		lengthStr = strlen(inputStr) - 1;
 
-	for (int i = 0; i < lengthStr; i++) {
-		char nowLetter = inputStr[i];
-		if (_wasChange) {
-			_internalPosition = 0;
-			_wasChange = 0;
-		}
-		if (isdigit(inputStr[i])) {
-			_lastIs = 'd';
-			digitList[digitList_position][_internalPosition] = nowLetter;
-			digitList[digitList_position][_internalPosition + 1] = '\0';
-		}
-		else {
-			_lastIs = 'l';
-			letterList[letterList_position][_internalPosition] = nowLetter;
-			letterList[letterList_position][_internalPosition + 1] = '\0';
-		}
-		if (i != lengthStr - 1) {
-			if (isdigit(inputStr[i]) != isdigit(inputStr[i + 1])) {
+		int _internalPosition = 0;
+		int _wasChange = 0;
+		char _lastIs;
+
+		for (int i = 0; i < lengthStr; i++) {
+			char nowLetter = inputStr[i];
+			if (_wasChange) {
+				_internalPosition = 0;
+				_wasChange = 0;
+			}
+			if (isdigit(inputStr[i])) {
+				_lastIs = 'd';
+				digitList[digitList_position][_internalPosition] = nowLetter;
+				digitList[digitList_position][_internalPosition + 1] = '\0';
+			}
+			else {
+				_lastIs = 'l';
+				letterList[letterList_position][_internalPosition] = nowLetter;
+				letterList[letterList_position][_internalPosition + 1] = '\0';
+			}
+			if (i != lengthStr - 1) {
+				if (isdigit(inputStr[i]) != isdigit(inputStr[i + 1])) {
+					if (_lastIs == 'd') digitList_position++;
+					else letterList_position++;
+					_wasChange = 1;
+				}
+			}
+			else {
 				if (_lastIs == 'd') digitList_position++;
 				else letterList_position++;
-				_wasChange = 1;
 			}
+			_internalPosition++;
 		}
+
+		printf("\n");
+		if (letterList_position == 0) printf("Во входной строке нет букв\n");
 		else {
-			if (_lastIs == 'd') digitList_position++;
-			else letterList_position++;
-		}
-		_internalPosition++;
-	}
-
-	printf("\n");
-	if (letterList_position == 0) printf("Во входной строке нет букв\n");
-	else {
-		printf("\nМассив из последовательностей букв: [ ");
-		for (int i = 0; i < letterList_position; i++) {
-			int sectionLength = strlen(letterList[i]);
-			for (int s = 0; s < sectionLength; s++) {
-				printf("%c", letterList[i][s]);
+			printf("Массив из последовательностей букв: [ ");
+			for (int i = 0; i < letterList_position; i++) {
+				int sectionLength = strlen(letterList[i]);
+				for (int s = 0; s < sectionLength; s++) {
+					printf("%c", letterList[i][s]);
+				}
+				printf(", ");
 			}
-			printf(", ");
+			printf("]\n");
 		}
-		printf("]\n");
-	}
 
-	if (digitList_position == 0) printf("Во входной строке нет цифр\n");
-	else {
-		printf("Массив из последовательностей цифр: [ ");
-		for (int i = 0; i < digitList_position; i++) {
-			int sectionLength = strlen(digitList[i]);
-			for (int s = 0; s < sectionLength; s++) {
-				printf("%c", digitList[i][s]);
+		if (digitList_position == 0) printf("Во входной строке нет цифр\n");
+		else {
+			printf("Массив из последовательностей цифр: [ ");
+			for (int i = 0; i < digitList_position; i++) {
+				int sectionLength = strlen(digitList[i]);
+				for (int s = 0; s < sectionLength; s++) {
+					printf("%c", digitList[i][s]);
+				}
+				printf(", ");
 			}
-			printf(", ");
+			printf("]\n");
 		}
-		printf("]\n");
+
+		printf("\nВведите 1 - заново, 0 - выйти: ");
+		scanf("%d", &cont);
+		printf("\n");
+		scanf("%c", &trash);
 	}
 	return 0;
 }
